45198063001ae7e8da8e673883d83d847f2ff777
rudymackenzi <147726669+rudymackenzi@users.noreply.github.com>
2024-02-26 14:22:13 +0300

    Add files via upload

---
+++ stock/src/Application/Parameter.java
@@ -0,0 +1,13 @@
+
+package Application;
+
+
+public class Parameter {
+    public static String IPHOST = "127.0.0.1"; //adresse de bouclage
+    public static String HOST_DB = "jdbc:mysql;//" + IPHOST + ":3306/stock"; //notre base de donné
+    public static String USERNAME_DB = "root";
+    public static String PASSWORD_DB = ""; //"root"
+    
+    public static int PORT = 11111;
+    public static String USER;
+}
